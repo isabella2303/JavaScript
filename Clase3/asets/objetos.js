f823f71b0bc5423836c692d24127170429204e9f
isabella2303 <isabellazapatapulgarin@gmail.com>
2022-07-21 09:19:10 -0500

    Esto hace parte de la clase3

---
+++ Clase3/asets/objetos.js
@@ -0,0 +1,26 @@
+//Crear objeto vacio
+let objeto={}
+console.log(objeto)
+//Creando objeto carro
+let carro={
+    color:"rojo",
+    velocidadMaxima:"100 km/h",
+    modelo:2020,
+    placa:"ISA2330",
+    dueños:["Isabella", "Sofia"],
+    lugarCreacion:{
+        ciudad:"Medellin",
+        pais:"Colombia",
+    },
+    tieneSoat:true
+}
+console.log(carro)
+
+let cuaderno={
+    color:"multicolor",
+    valor:3000,
+    marca:"Ay Dios!",
+    productoHecho:"Colombia",
+    tipo:"Rayado",
+}
+console.log(cuaderno)
