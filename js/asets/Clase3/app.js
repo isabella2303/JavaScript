dda6926b6b75a9164708b19b4609567fe55f079a
isabella2303 <isabellazapatapulgarin@gmail.com>
2022-07-21 08:10:09 -0500

    Esta es la clase 3

---
+++ js/asets/Clase3/app.js
@@ -0,0 +1,13 @@
+//crear array vacio
+let array=[];
+
+console.log(array);
+//Creamos cuatro vectores con diferentes valores
+let numeros=[1,2,3,4]
+//console.log(numeros)
+let animales=["perro","gato","loro","pato","sapo"]
+//console.log(animales)
+let barrios=["San Pio", "Yarumito", "Ditaires", "San Jose"]
+//console.log(barrios)
+let celulares=["Samsung", "Huawei", "Vivo", "Poco"]
+console.log(celulares.length)
