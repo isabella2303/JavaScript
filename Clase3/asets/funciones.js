ac20e881150d91fd55561f7e43de2e1c57b4619d
isabella2303 <isabellazapatapulgarin@gmail.com>
2022-07-21 09:59:46 -0500

    Hace parte de la clase3

---
+++ Clase3/asets/funciones.js
@@ -0,0 +1,38 @@
+//funcion normal
+function saludar(){
+console.log("Hello Everybody")
+}
+
+saludar();
+saludar();
+saludar();
+
+function sumar(){
+    let suma=2+3;
+    console.log(suma)
+}
+saludar()
+sumar()
+
+function restar(){
+    let restar=15*8;
+    console.log(restar)
+}
+restar()
+
+//funcion tipo flecha
+const saludo=()=>{
+}
+console.log(`Hola ${nombre}`)
+saludo("Sofia");
+saludo("Isabella");
+saludo("Ticher");
+
+const suma=(num1,num2)=>{
+    let resultado=num1+num2;
+    console.log(resultado)
+}
+
+suma(5,10)
+suma(25,36)
+
