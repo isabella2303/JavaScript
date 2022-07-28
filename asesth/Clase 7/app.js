93111121a309f519b62e2bfc8939c38a8aedffb5
isabella2303 <isabellazapatapulgarin@gmail.com>
2022-07-28 12:25:10 -0500

    Clase7, primera parte

---
+++ asesth/Clase 7/app.js
@@ -0,0 +1,16 @@
+//AQUI VIMOS El OPERADOR &&
+const entrarAlBar = (edad, pago) => {
+  if (edad >= 18 && pago == true) {
+    console.log('Puede entrar al Bar')
+  } else {
+    console.log('No puede entrar al Bar')
+  }
+}
+let edadPersona = Number(prompt('Ingrese su edad'))
+let pagoEntrada = Boolean(prompt('¿Usted pago?'))
+entrarAlBar(edadPersona, pagoEntrada)
+
+console.log(true & true)
+console.log(true & false)
+console.log(false & true)
+console.log(false & false)
