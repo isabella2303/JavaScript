1dbc3eb2f78d1e97597bdf8101c96025da2cdb59
isabella2303 <isabellazapatapulgarin@gmail.com>
2022-08-04 07:34:17 -0500

    Como obtener datos con el nombre de la etiqueta

---
+++ CLase 9/asesth/alcance.js
@@ -0,0 +1,20 @@
+let num=1;
+
+console.log(num)
+
+const saludar=()=>{
+    let num2=5;
+    console.log(num*num2)
+}
+
+saludar()
+
+const validarEdad=()=>{
+    let edad=19;
+    if (edad>18) {
+        let mensaje="Wakanda my suggar momy"
+        console.log("Es mayor de edad")
+        console.log(mensaje)
+    }else{
+        console.log("Tienes colageno")
+}
---
+++ Clase10/scripts/app.js
@@ -0,0 +1,7 @@
+//Obtener elementos con la funcion getElementsByTagName
+let todosLosTitulos=document.getElementsByTagName(`h1`)
+
+console.log(todosLosTitulos)
+
+let todosLosSubtitulos=document.getElementsByTagName(`h2`)
+console.log(todosLosSubtitulos)
