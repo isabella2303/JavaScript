753a68027864cb90b5ed1ac77556132b77002be8
isabella2303 <isabellazapatapulgarin@gmail.com>
2022-08-11 07:27:58 -0500

    Aqui vimos como manejar los errores

---
+++ Clase 13/scripts/app.js
@@ -0,0 +1,10 @@
+try {
+    const nombre= "Isabella"
+    const apellidos= "Zapata Pulgarin";
+const nombreCompleto= nombre + " " + apellidos;
+
+console.log(nombreCompleto);
+
+}catch (e){
+    console.log(e)
+}
---
+++ Clase 13/scripts/errores.js
@@ -0,0 +1,14 @@
+const obtenerNumero=()=>{
+    let num=prompt("Ingrese un numero");
+    try{
+        if (num == "") throw "Esta vacio";
+        if (isNaN(num)) throw "No es un numero";
+        num=Number(num);
+        if (num <5) throw "El numero es muy pequeño";
+        if (num >10) throw "El numero es muy grande";
+    }catch (error){
+        console.log(error);
+
+    }
+};
+obtenerNumero()
